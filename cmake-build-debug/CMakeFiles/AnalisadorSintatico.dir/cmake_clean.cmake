9e1708c90eaafb9762a1f12f30ac8d95b69eab5a
Lucas <lucasmartioli12@hotmail.com>
2016-12-27 23:30:20 -0200

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/AnalisadorSintatico.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/AnalisadorSintatico.dir/main.cpp.obj"
+  "AnalisadorSintatico.pdb"
+  "AnalisadorSintatico.exe"
+  "AnalisadorSintatico.exe.manifest"
+  "libAnalisadorSintatico.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/AnalisadorSintatico.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
